ca51e41f759fc594663c86d0c79e94f4e8328692
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-27 23:24:07 +0400

    changed initial state

--- src/Context/globalcontext.jsx
+++ src/Context/globalcontext.jsx
@@ -1,22 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+
+const initialState = {
+  photo: "",
+  name: "",
+};
 
 const GlobalContext = React.createContext();
 const GlobalContextProvider = ({ children }) => {
-  const addPhoto = (e) => {
+  const [user, setUser] = useState(initialState);
+  console.log(user);
+  const uploadPhoto = (e) => {
     const file = e.target.files[0];
     const reader = new FileReader();
     reader.readAsDataURL(file);
-
     reader.onload = () => {
-      setFormValues({
-        ...formValues,
-        image: reader.result,
-      });
+      const base64String = reader.result;
+      setUser({ ...user, photo: base64String });
     };
   };
-
+  
+  const handleUserChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
   return (
-    <GlobalContext.Provider value={{ addPhoto }}>
+    <GlobalContext.Provider value={{ uploadPhoto, handleUserChange, user }}>
       {children}
     </GlobalContext.Provider>
   );
