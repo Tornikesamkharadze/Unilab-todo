9e652fddd5344186a0c919888989600c3128f10f
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-27 23:23:11 +0400

    rendering user photo if its exists

--- src/Routes/TodoCart.jsx
+++ src/Routes/TodoCart.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useGlobalContext } from "../Context/globalcontext";
 
 const TodoCart = () => {
-  return <div>TodoCart</div>;
+  const { user } = useGlobalContext();
+
+  return (
+    <div>
+      {user.photo && (
+        <img
+          style={{ width: "100px", height: "100px" }}
+          src={user.photo}
+          alt="user_image"
+        />
+      )}
+      TodoCart
+    </div>
+  );
 };
 
 export default TodoCart;
