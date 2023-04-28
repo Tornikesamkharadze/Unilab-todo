f7eaa1c1c50550ba9823c3e53d3c11937872144b
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-29 02:10:43 +0400

    wrapped app in TodoContextProvider provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { GlobalContextProvider } from "./Context/globalcontext.jsx";
+import { TodoContextProvider } from "./Context/todoContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GlobalContextProvider>
-      <App />
+      <TodoContextProvider>
+        <App />
+      </TodoContextProvider>
     </GlobalContextProvider>
   </React.StrictMode>
 );
