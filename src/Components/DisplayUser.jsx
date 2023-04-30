a8cf2335a468a663554950fb8bbace518d8df9b6
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-30 15:20:59 +0400

    created styles for logout popup

--- src/Components/DisplayUser.jsx
+++ src/Components/DisplayUser.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 import { useGlobalContext } from "../Context/globalcontext";
 import { useTodoContext } from "../Context/todoContext";
 
+import { GrClose } from "react-icons/gr";
 import styled from "styled-components";
 
 const DisplayUser = () => {
@@ -25,7 +25,20 @@ const DisplayUser = () => {
       {user.photo && (
         <div className="image-container">
           <img src={user.photo} alt="user_photo" onClick={toggleHandler} />
-          {logOut && <button onClick={logOutHandler}>Logout</button>}
+          {logOut && (
+            <div className="popup">
+              <div className="popup-content">
+                <p className="popup-text">
+                  <span> notice!!!</span> if you logout information whant you
+                  entered will be deleted
+                </p>
+                <GrClose className="popup-close" onClick={toggleHandler} />
+                <button className="popup-logout" onClick={logOutHandler}>
+                  Logout
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       )}
     </UserWrapper>
@@ -40,22 +53,73 @@ const UserWrapper = styled.div`
     .image-container {
       display: flex;
       align-items: center;
-      button {
-        margin-left: 10px;
-      }
       img {
         height: 50px;
         width: 50px;
         border-radius: 50px;
       }
     }
+    .popup {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.5);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .popup-content {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      background-color: #fff;
+      padding: 38px;
+      border-radius: 10px;
+      width: 50%;
+      max-width: 500px;
+    }
+    .popup-text {
+      color: black;
+      text-align: center;
+      margin: 0;
+      span {
+        color: red;
+      }
+    }
+    .popup-close {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      cursor: pointer;
+      font-size: 20px;
+    }
+    .popup-logout {
+      margin-top: 20px;
+      background-color: #5efc8d;
+      font-size: 20px;
+      color: black;
+      border: none;
+      border-radius: 5px;
+      padding: 10px 20px;
+      cursor: pointer;
+    }
+    .popup-logout:hover {
+      background-color: black;
+      color: white;
+      transition: background-color 0.5s ease-out, color 0.9s ease-out;
+      border-radius: 4px;
+    }
   }
   @media (min-width: 500px) {
     gap: 20px;
+    p {
+      font-size: 22px;
+    }
     .image-container {
-      button {
-        margin-left: 20px;
-      }
       img {
         height: 68px;
         width: 68px;
