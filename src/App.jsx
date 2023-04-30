927dd94ff502c4982bdcc2fd2240792046d60b2a
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-30 23:45:15 +0400

    imported errore page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Routes/Home";
 import SignInForm from "./Routes/SignInForm";
 import TodoCart from "./Routes/TodoCart";
+import ErrorPage from "./Routes/ErrorPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route index="/" element={<Home />} />
         <Route path="signinform" element={<SignInForm />} />
         <Route path="todocart" element={<TodoCart />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
