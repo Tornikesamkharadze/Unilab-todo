f4d0a158679baf8c5086b1fd3117ed05cbc4a487
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-05-01 23:53:24 +0400

    imported protectedroutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Routes/Home";
-import SignInForm from "./Routes/SignInForm";
-import TodoCart from "./Routes/TodoCart";
+import ProtectedSignInForm from "./ProtectedRoutes/ProtectedSignInForm";
+import ProtectedTodoCart from "./ProtectedRoutes/ProtectedTodoCart";
 import ErrorPage from "./Routes/ErrorPage";
+import ProtectedHome from "./ProtectedRoutes/ProtectedHome";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index="/" element={<Home />} />
-        <Route path="signinform" element={<SignInForm />} />
-        <Route path="todocart" element={<TodoCart />} />
+        <Route index="/" element={<ProtectedHome />} />
+        <Route path="/signinform" element={<ProtectedSignInForm />} />
+        <Route path="/todocart" element={<ProtectedTodoCart />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
